91a06902a5ce99789a0793ff9c48e05618dad426
Samuel Terra <samuelterra22@hotmail.com>
2019-08-06 14:45:47 -0300

    Initial commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import { Container, Title, Form, Input, Submit } from './styles'
+
+export default function Main () {
+  return (
+    <Container>
+      <Title>Repositórios</Title>
+
+      <Form>
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Procurar repositório..."
+        />
+        <Submit onPress={() => {}}>
+          <Icon name="add" size={22} color="#FFF" />
+        </Submit>
+      </Form>
+    </Container>
+  )
+}
